1d93f6e338d0e482daa5713050119a0d3b8e1074
Prumme <aurelien23.p@gmail.com>
2023-04-10 23:17:14 +0200

    fix reset array

--- src/Components/Sorts.jsx
+++ src/Components/Sorts.jsx
@@ -1,12 +1,61 @@
 function Sorts({ array, setArray }) {
     const reset = () => {
-        setArray(array.sort((a, b) => 0.5 - Math.random()));
+        setArray((prev) => {
+            return [...prev].sort(() => Math.random() - 0.5)
+        }
+        )
     }
+
+    // const bubbleSort = () => {
+    //     let temp = [...array]
+    //     let len = temp.length
+    //     for (let i = 0; i < len; i++) {
+    //         for (let j = 0; j < len; j++) {
+
+
+    //             if (temp[j] > temp[j + 1]) {
+    //                 let tmp = temp[j];
+    //                 temp[j] = temp[j + 1];
+    //                 temp[j + 1] = tmp;
+    //                 setArray([...temp])
+    //                 return;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // const sorted = () => {
+    //     for (let i = 0; i < array.length() - 1; i++) {
+    //         if (array[i] > array[i + 1]) {
+    //             return false
+    //         }
+    //     }
+    //     return true
+    // }
+
+
+
+
+    // function sleep(milliseconds) {
+    //     const date = Date.now();
+    //     let currentDate = null;
+    //     do {
+    //         currentDate = Date.now();
+    //     } while (currentDate - date < milliseconds);
+    // }
+
+
     return (
         <div className="sorts">
             <ul className="flex space-x-4">
                 <li>
-                    <button className="px-2 py-1 border border-[#232ed1] hover:scale-110 duration-300 rounded-lg text-[#232ed1]">Bubble sort</button>
+                    <button onClick={() => {
+                        // while (sorted) {
+                        //     console.table(array)
+                        //     bubbleSort()
+                        //     sleep(1000)
+                        // }
+                    }} className="px-2 py-1 border border-[#232ed1] hover:scale-110 duration-300 rounded-lg text-[#232ed1]">Bubble sort</button>
                 </li>
                 <li>
                     <button className="px-2 py-1 border border-[#232ed1] hover:scale-110 duration-300 rounded-lg text-[#232ed1]">Cocktail sort</button>
